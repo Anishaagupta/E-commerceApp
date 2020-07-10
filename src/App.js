d83384cdb93521879bb0e5f0d7625b0e21851f1b
Anisha <anisha8057@gmail.com>
2020-07-11 01:37:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './Pages/homePage/HomePage.component'
+
+function App() {
+  return(
+    <div className='App'>
+      <HomePage/>
+    
+    </div>
+  )
+}
+
+export default App;
